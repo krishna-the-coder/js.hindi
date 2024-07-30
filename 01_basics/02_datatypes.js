1cf92683e5a4b0be59bf7a262c80f8221082a937
Krishna Kumar <kk1904822@gmail.com>
2024-07-30 02:26:56 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3 + 3) //we are using nodejs, not browser
+
+console.log(3
+    +
+    3)  // code readability should be high, this is wrong practice of writing code 
+
+console.log("Github")
+
+
+let name = "Krishna"
+let age = 22
+let isLoggedIn = false
+let state; //value not define/assign, only variable declared
+
+// number => 2 to power 53
+// bigint = use in big site like- facebook,reddit or in trading,stock
+// String => ""
+// boolean => true/false
+// null => standalone value  //Represent empty value
+// undefined => 
+// symbol => unique     //mostly use in react in component
+
+
+// object
+
+console.log(typeof "string") //type is string
+console.log(typeof age) //type is number- Example - 22
+console.log(typeof undefined);  // undefined
+console.log(typeof null);  // object

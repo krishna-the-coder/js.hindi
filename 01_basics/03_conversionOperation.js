dc7789a66c31a71c0113a4f0915c3231260b0b24
Krishna Kumar <kk1904822@gmail.com>
2024-07-31 02:22:28 +0000

    conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -29,5 +29,49 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value   //negvalue(negative value)
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(5%2);
+
+let str1 = "hello"
+let str2 = " krishna"
+
+let str3 = str1 + str2
+// console.log(str3);      //string add
+
+// console.log("1" + 2);   // 12
+// console.log(1 + "2");   // 12
+// console.log("1" + 2 + 2);   // 122
+// console.log(1 + 2 + "2");   // 32 bcoz of ToPrimitive
+
+// console.log( (3 + 4) * 5 % 3);       // confusing operation, not readable code
+
+// console.log(+true);     // 1 bcoz of increment in boolean
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2      // confusing code not clean
+// console.log(num1, num2, num3);
+
+let gameCounter = 100
+// gameCounter++;      //posfix operator
+++gameCounter;      //prefix operator
+console.log(gameCounter);
+
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
